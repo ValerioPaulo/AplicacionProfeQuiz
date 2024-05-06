b9dde4a011036799cdced4bad624481c14fd03e1
Paulo <prueba@hotmail.com>
2024-05-05 19:34:50 -0500

    Initial commit

---
+++ app/src/main/java/com/valerio/android/aplicacionprofequiz/Vista/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.valerio.android.aplicacionprofequiz.Vista;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.valerio.android.aplicacionprofequiz.R;
+import com.valerio.android.aplicacionprofequiz.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding mActivityLoginBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        //setContentView(R.layout.activity_login);
+        setContentView(mActivityLoginBinding.getRoot());
+
+        mActivityLoginBinding.iniciarSesionLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+}
