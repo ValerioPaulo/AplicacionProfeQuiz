cdbab56302663ae4d0d75da5078650f41454b5dd
ch29aaron <157434584+ch29aaron@users.noreply.github.com>
2024-05-31 12:39:50 -0500

    el card envia a la ventana profesor

--- app/src/main/java/com/valerio/android/aplicacionprofequiz/Vista/DetailActivity.java
+++ app/src/main/java/com/valerio/android/aplicacionprofequiz/Vista/DetailActivity.java
@@ -13,19 +13,21 @@ import com.valerio.android.aplicacionprofequiz.Vista.modelo.Top;
 public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.fragment_perfil_docente);
 
         Top top = (Top) getIntent().getSerializableExtra("top"); // Obtener el objeto Top del intent
 
-        TextView nombreCompleto = findViewById(R.id.nombre_completo);
-        TextView correoProf = findViewById(R.id.correo_prof);
-        ImageView fotoProf = findViewById(R.id.foto_prof);
-        TextView carreraProf = findViewById(R.id.carrera_prof);
-        TextView calificacion = findViewById(R.id.calificacion);
+        TextView nombreCompleto = findViewById(R.id.NombreDato);
+        TextView nombreCompleto1 = findViewById(R.id.NombreCompletoDato);
+        TextView correoProf = findViewById(R.id.CorreoDato);
+        ImageView fotoProf = findViewById(R.id.fot);
+        TextView carreraProf = findViewById(R.id.CarreraDato);
+        TextView calificacion = findViewById(R.id.cal);
 
         // Asignar valores a los elementos de la vista
 
         nombreCompleto.setText(top.getNombre_completo());
+        nombreCompleto1.setText(top.getNombre_completo());
         correoProf.setText(top.getCorreo_prof());
         Glide.with(this).load(top.getFot_prof()).into(fotoProf);
         carreraProf.setText(top.getCarrera_prof());
